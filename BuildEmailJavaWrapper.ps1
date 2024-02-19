3ae5ac8203af8aee3d4a8dc971afe163fd5134ca
Melissa <ha@melissadata.com>
2024-02-19 13:17:27 -0800

    Tested successfully using Melissa object for 2024.01

--- BuildEmailJavaWrapper.ps1
+++ BuildEmailJavaWrapper.ps1
@@ -54,6 +54,14 @@ $Files = @(
     Type           = "INTERFACE";
   },
   [FileConfig]@{
+    FileName       = "mdEmailJavaWrapper.cpp";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "ANY";
+    Compiler       = "JAVA";
+    Architecture   = "ANY";
+    Type           = "INTERFACE";
+  },
+  [FileConfig]@{
     FileName       = "mdEmail_JavaCode.zip";
     ReleaseVersion = $RELEASE_VERSION;
     OS             = "ANY";
